1ed6d05acaa16ea394c067908d77c481879dcf21
PhDNoe <nigth.crawler25@gmail.com>
2023-03-29 01:14:48 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,101 @@
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+
+# **Proyecto de Noelia**  👻
+
+# 📈 Factibilidad de cartera de inversión
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+
+---
+## Empresa solicitante: Ficticia Conservadora (F.C)
+---
+
+
+### La empresa Ficticia Conservadora  ha solicitado una recomendación de cartera de inversión.
+
+> * La empresa, una startup en crecimiento, desea invertir un capital excedente.
+> * Acepta correr un riesgo mínimo a cambio de una tasa de retorno superior a la tasa de inflación
+de Estados Unidos de los ultimos 5 años.
+> * Su horizonte de inversión es a mediano/largo plazo, con una estancia mínima de 2 años.
+
+
+
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+## Contenido del informe:
+
+### 📈 Análisis general
+Dashboard interactivo mostrando evolución del índice S&P500 durante los últimos 23 años y de las compañías que lo componen.
+    - 🗓 Fitros de fechas
+    - 🫧 Indicadores gráficos de 3 crisis económicas:
+        - 🌐 Burbuja dot com  
+        - 🏠 Burbuja de hipotecas subprime
+        - 🦠 Pandemia Covid-19  
+
+<img src ="./img/general.png" width="500px">
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+
+### 📈 Análisis descriptivo y estadístico por sectores
+Dashboard interactivo mostrando la evolución de los últimos 23 años de los distintos sectores
+    - 🗓  Filtro de fechas
+    - 📈 Datos de inflación anual y acumulada
+
+<img src ="./img/sector.png" width="500px">
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+
+### 📈 Análisis descriptivo y estadístico de las empresas seleccionadas
+Dashboard interactivo mostrando la evolución de los últimos 5 años de las distintas empresas
+    - 📈 Ganancia porcentual por empresa
+    - 📈 Datos de inflación anual y acumulada
+    - 📈 Fundamentales de las empresas
+    
+<img src ="./img/empresas.png" width="500px">
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+
+### 📈 Análisis técnico de las compañias recomendadas
+Dashboard interactivo mostrando los principales indicadores técnicos para analizar posibles zonas de compra.
+
+Indicadores disponibles:
+- 📉 Medias moviles de 50 y 100 días
+- 📉 Índice de fuerza relativa (RSI)
+
+<img src ="./img/tecnico.png" width="500px">
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
+
+### 📈 Conclusiones y recomendación final
+
+#### **Conclusiones**
+
+La recomendación final es armar una cartera equilibrada con aquellas acciones (de la lista de preseleccionadas) que cumplan con la condición de:
+
+- Superar el porcentaje de inflación acumulado de los últimos 5 años
+- Superar el porcentaje promedio del sp500 de los ultimos 5 años
+
+
+Se recomienda esperar una señal clara de compra para las siguientes acciones:
+
+- Costco (COST)
+- General Mills (GIS)
+- Hershey's (HSI)
+- Coca-Cola (KO)
+- Mondelez International (MDLZ)
+- Monster Beverage (MNST)
+- PepsiCo (PEP)
+- Walmart (WMT)
+
+Debido a que superan el margen de ganancia general del S&P500, y ademas han mostrado ser robustas frente a todas las crisis economicas.
+
+<img src ="./img/costco.png" width="500px">
+
+#### En que invertir mientras no haya señal de compra?
+
+- Bonos del tesoro
+- Cauciones bursátiles
+
+> Si tienen confianza en el mundo crypto, pueden esperar en un stacking flexible con alguna stablecoin tipo USDT o similar.
+
+![divider](https://user-images.githubusercontent.com/7065401/52071927-c1cd7100-2562-11e9-908a-dde91ba14e59.png)
--- StreamLitPractice/pages/4_📈_Analisis_Compañias_Elegidas.py
+++ StreamLitPractice/pages/4_📈_Analisis_Compañias_Elegidas.py
@@ -25,6 +25,26 @@ spy_5 = pd.read_csv('./data/spy_5.csv')
 spy_5["Date"] =  pd.to_datetime(spy_5["Date"], format="%Y-%m-%d")   
 
 
+def show_fundamentales(company_opt):
+    my_dict = {
+        'Colgate-Palmolive': """Colgate-Palmolive Company is a consumer products company that markets 
+        its products throughout the world. 
+        The Company's products include toothpaste, toothbrushes, shampoos, deodorants, bar and liquid soaps, dishwashing liquid, and laundry products, as well as pet nutrition products for cats and dogs."""
+        , "Costco":"""Costco Wholesale Corporation is a membership warehouse club The Company sells all kinds of food, automotive supplies, toys, hardware, sporting goods, jewelry, electronics, apparel, health, and beauty aids, as well as other goods. Costco Wholesale serves customers worldwide."""
+        , "General Mills":"""General Mills, Inc. operates as a food company. The Company manufactures and markets branded consumer foods. General Mills serves customers worldwide."""
+        , "Hershey's":"""The Hershey Company manufactures chocolate and sugar confectionery products. The Company's principal products includes chocolate and sugar confectionery products, gum and mint refreshment products, and pantry items, such as baking ingredients, toppings, and beverages."""
+        , "Kimberly-Clark":"""Kimberly-Clark Corporation is a global health and hygiene company that manufactures and provides consumer products. The Company's products include diapers, tissues, paper towels, incontinence care products, surgical gowns, and disposable face masks. Kimberly-Clark's products are sold in countries around the world."""
+        , "Coca-Cola company": """The Coca-Cola Company manufactures, markets, and distributes soft drink concentrates and syrups. The Company also distributes and markets juice and juice-drink products. Coca-Cola distributes its products to retailers and wholesalers worldwide."""
+        , "Mondelez International": """Mondelez International, Inc. is a food and beverage company. The Company manufactures and markets packaged food products, including snacks, beverages, cheese, convenient meals, and other packaged grocery products. Mondelez International sells its products worldwide."""
+        , "Monster Beverage":"""Monster Beverage Corporation operates as a holding company. The Company, through its subsidiaries, markets and distributes energy drinks. Monster Beverage serves customers worldwide."""
+        , "PepsiCo": """PepsiCo, Inc. operates foods and beverages businesses. The Company manufactures markets and sells a variety of grain-based snacks, carbonated and non-carbonated beverages, and foods. PepsiCo serves customers worldwide."""
+        , "Walmart": """Walmart Inc. operates discount stores, supercenters, and neighborhood markets. The Company offers merchandise such as apparel, house wares, small appliances, electronics, musical instruments, books, home improvement, shoes, jewelry, toddler, games, household essentials, pets, pharmaceutical products, party supplies, and automotive tools. Walmart serves customers worldwide"""
+    
+        
+    }
+
+    st.markdown(f"### {company_opt}")
+    st.markdown(my_dict[company_opt])
 
 
 company_opt = company_opt(get_selected)
@@ -32,14 +52,15 @@ company_opt = company_opt(get_selected)
 col1, col2 = st.columns([2,1])
 
 with col1:
-    tab1, tab2, tab3 = st.tabs([f"Evolución Spy vs {company_opt}", f"Evolución {company_opt}", "Evolución SPY"])
+    tab1, tab2, tab3, tab4 = st.tabs([f"Evolución Spy vs {company_opt}", f"Evolución {company_opt}", "Evolución SPY", "Info"])
     with tab1:
         plot_spy_selected(company_opt, get_selected, 0, spy_5, df_5, get_metrics)
     with tab2:
         plot_spy_selected(company_opt, get_selected, 1, spy_5, df_5, get_metrics)
     with tab3:
         plot_spy_selected(company_opt, get_selected, 2, spy_5, df_5, get_metrics)
-
+    with tab4:
+        show_fundamentales(company_opt)
 with col2:
     plot_metrics(spy_5, df_5, company_opt, get_selected, get_metrics)
 
